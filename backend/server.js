abde05c947ba29307408eb890777bf40a036f961
Lionel <ayooladeji8@gmail.com>
2026-04-23 12:08:19 +0400

    Remove trailing slash from CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://leo-bank-tau.vercel.app/"
+    "https://leo-bank-tau.vercel.app"
   ],
   credentials: true
 }))
